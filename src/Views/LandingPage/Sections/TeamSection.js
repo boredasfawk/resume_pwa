0c3f0aabe2019f03315aef9ea88247eac0da0a54
boredasfawk <olonnyetaylor@gmail.com>
2020-03-20 22:41:54 -0700

    test

--- src/Views/LandingPage/Sections/TeamSection.js
+++ src/Views/LandingPage/Sections/TeamSection.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import * as THREE from 'three';
 import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
-import { OBJLoader } from "three/examples/js/loaders/OBJLoader";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import Stats from 'stats.js';
 import classNames from "classnames";
 // @material-ui/core components
